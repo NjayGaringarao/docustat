6c43a0a314f56a656bc0eed636079293d7f9aca2
John Paul <johnpaulmarquez028@gmail.com>
2025-02-09 21:13:42 +0800

    chore: modify Tailwind theme colors (primary, background, secondary, panel)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#DDA853",
-        background: "#FBF5DD",
-        secondary: "#16404D",
-        panel: "#A6CDC6",
+        primary: "#F5F5DC",
+        background: "#E6E6FA",
+        secondary: "#66CDAA",
+        panel: "#FFFDD0",
         uBlack: "#1f2937",
         uGray: "#4b5563",
         uGrayLight: "#9ca3af",
