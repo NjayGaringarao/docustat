848b09993309545423f726dce2613af737adc022
John Paul <johnpaulmarquez028@gmail.com>
2025-02-10 14:56:32 +0800

    chore: update color.ts to match Tailwind theme (secondary & uGrayLight updated)

--- constants/color.ts
+++ constants/color.ts
@@ -1,11 +1,11 @@
 export const color = {
   primary: "#F5F5DC",
   background: "#E6E6FA",
-  secondary: "#66CDAA",
+  secondary: "#40B5AD",
   panel: "#FFFDD0",
   uBlack: "#1f2937",
   uGray: "#4b5563",
-  uGrayLight: "#9ca3af",
+  uGrayLight: "#36454F",
   success: "#10B981",
   failed: "#dc2626",
   white: "#fff",
