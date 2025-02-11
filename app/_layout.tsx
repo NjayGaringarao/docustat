30c3b1f4e066328078bf9f04a5457936098bc3e2
njay <ninogaringarao13@gmail.com>
2025-02-11 23:20:05 +0800

    Modified (_layout.tsx) : add toastConfig

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
-import { useEffect } from "react";
+import { JSX, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Toast from "react-native-toast-message";
+import Toast, {
+  BaseToast,
+  BaseToastProps,
+  ErrorToast,
+} from "react-native-toast-message";
 import "../global.css";
 import GlobalProvider from "@/context/GlobalProvider";
+import { color } from "@/constants/color";
+import { View, Text } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -12,6 +19,112 @@ export default function RootLayout() {
     Merriweather: require("@/assets/fonts/merriweather/Merriweather-Bold.ttf"),
     Mono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  const toastConfig = {
+    success: ({ text1, text2 }: BaseToastProps) => (
+      <View
+        style={{
+          height: 96,
+          width: "100%",
+          paddingHorizontal: 16,
+          marginTop: 64,
+        }}
+      >
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: color.background,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 16,
+            borderRadius: 12,
+            overflow: "hidden",
+          }}
+        >
+          <View
+            style={{
+              height: 96,
+              width: 96,
+              backgroundColor: color.success,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <AntDesign name="checkcircle" size={42} color={color.white} />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              gap: 4,
+            }}
+          >
+            <Text
+              style={{ fontSize: 18, fontWeight: "600", color: color.uBlack }}
+            >
+              {text1}
+            </Text>
+            <Text style={{ fontSize: 12, color: color.uBlack, lineHeight: 12 }}>
+              {text2}
+            </Text>
+          </View>
+        </View>
+      </View>
+    ),
+
+    error: ({ text1, text2 }: BaseToastProps) => (
+      <View
+        style={{
+          height: 96,
+          width: "100%",
+          paddingHorizontal: 16,
+          marginTop: 64,
+        }}
+      >
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: color.background,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 16,
+            borderRadius: 12,
+            overflow: "hidden",
+          }}
+        >
+          <View
+            style={{
+              height: 96,
+              width: 96,
+              backgroundColor: color.failed,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <AntDesign name="exclamationcircle" size={42} color={color.white} />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              gap: 4,
+            }}
+          >
+            <Text
+              style={{ fontSize: 18, fontWeight: "600", color: color.uBlack }}
+            >
+              {text1}
+            </Text>
+            <Text style={{ fontSize: 12, color: color.uBlack, lineHeight: 12 }}>
+              {text2}
+            </Text>
+          </View>
+        </View>
+      </View>
+    ),
+  };
 
   useEffect(() => {
     if (loaded) {
@@ -37,7 +150,7 @@ export default function RootLayout() {
           />
         </Stack>
       </GlobalProvider>
-      <Toast />
+      <Toast config={toastConfig} />
     </SafeAreaView>
   );
 }
