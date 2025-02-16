1595c14e646591fb6fd4d1d8575d29c6da6cb324
John Paul <johnpaulmarquez028@gmail.com>
2025-02-16 20:52:50 +0800

    Refactor: Update Tailwind color palette for UI consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#DDA853",
+        primary: "#102542",
         background: "#FBF5DD",
-        secondary: "#16404D",
-        panel: "#A6CDC6",
+        secondary: "#F87060",
+        panel: "#928DAB",
         uBlack: "#1f2937",
         uGray: "#4b5563",
-        uGrayLight: "#9ca3af",
+        uGrayLight: "#36454F",
         success: "#10B981",
         failed: "#dc2626",
-        pending: "#9ca3af",
-        processing: "#3b82f6",
-        forPickup: "#f97316",
-        complete: "#22c55e",
       },
     },
   },
