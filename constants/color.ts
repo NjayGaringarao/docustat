58988e5ec0530e4b2845884d12a8af9d044eea8a
John Paul <johnpaulmarquez028@gmail.com>
2025-02-16 20:52:10 +0800

    Refactor: Adjust color scheme for improved UI consistency

--- constants/color.ts
+++ constants/color.ts
@@ -1,8 +1,8 @@
 export const color = {
-  primary: "#DDA853",
+  primary: "#102542",
   background: "#FBF5DD",
-  secondary: "#16404D",
-  panel: "#A6CDC6",
+  secondary: "#F87060",
+  panel: "#928DAB",
   uBlack: "#1f2937",
   uGray: "#4b5563",
   uGrayLight: "#9ca3af",
